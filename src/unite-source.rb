b672070e23c6c6d3beed910e21abd44c934980e6
miyazaki <miyazaki19851201@gmail.com>
2016-01-28 17:12:43 +0900

    modify to display abilities

--- src/unite-source.rb
+++ src/unite-source.rb
@@ -8,13 +8,6 @@ def has_gem?(gems, gem_name)
   gems.map {|e| e.name}.include?(gem_name)
 end
 
-type_hash = {'11' => 'くさ', '3' => 'どく', '9' => 'ほのお', '2' => 'ひこう',
-             '10' => 'みず', '6' => 'むし', '0' => 'ノーマル', '12' => 'でんき',
-             '4' => 'じめん', '17' => 'フェアリー', '1' => 'かくとう',
-             '13' => 'エスパー', '5' => 'いわ', '8' => 'はがね', '7' => 'ゴースト',
-             '14' => 'こおり', '15' => 'ドラゴン', '16' => 'あく'
-}
-
 hira = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゔぁぃぅぇぉっゃゅょー'
 kata = 'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴァィゥェォッャュョー'
 
@@ -38,7 +31,8 @@ open(this_dir + '/data.txt') do |f|
   obj = JSON.load data
   obj.each.with_index do |e, i|
     no = i + 1
-    types = e['types'].flatten
+    types = e['types']
+    abilities = e['abilities']
     name = e['name']
     if has_gem?(gems, 'romkan')
       hira = to_hira name
@@ -50,16 +44,12 @@ open(this_dir + '/data.txt') do |f|
     c = e['C']
     d = e['D']
     s = e['S']
-    types.map! do |t|
-      if type_hash[t]
-        type_hash[t]
-      else
-        t
-      end
-    end
     insert_str =  "'No.#{no}:\\ #{name}',\\ 'HP:\\ #{h}',\\ 'こうげき:\\ #{a}',\\ 'ぼうぎょ:\\ #{b}',\\ 'とくこう:\\ #{c}',\\ 'とくぼう:\\ #{d}',\\ 'すばやさ:\\ #{s}'"
     types.each.with_index do |t, j|
       insert_str += ",\\ 'タイプ#{j+1}:\\ #{t}'"
+    end
+    abilities.each.with_index do |ability, j|
+      insert_str += ",\\ '特性#{j+1}:\\ #{ability}'"
     end
     puts "No.#{no}: #{name}(#{roma})\t10new +call\\ append(0,[#{insert_str}])"
   end

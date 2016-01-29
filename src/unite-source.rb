b93e8237e84824edb68a9bf894bb4c415264f188
miyazaki <miyazaki19851201@gmail.com>
2016-01-29 13:40:51 +0900

    modify migemo

--- src/unite-source.rb
+++ src/unite-source.rb
@@ -1,22 +1,6 @@
 # coding: utf-8
 
 require 'json'
-require 'bundler'
-gems = Bundler.require
-
-def has_gem?(gems, gem_name)
-  gems.map {|e| e.name}.include?(gem_name)
-end
-
-hira = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゔぁぃぅぇぉっゃゅょー'
-kata = 'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴァィゥェォッャュョー'
-
-ary = [kata.each_char.to_a, hira.each_char.to_a].transpose
-KATA_TO_HIRA = Hash[*ary.flatten]
-
-def to_hira(str)
-  str.each_char.to_a.map {|c| KATA_TO_HIRA[c]}.join
-end
 
 this_dir = File.expand_path(File.dirname(__FILE__))
 open(this_dir + '/data.txt') do |f|
@@ -34,10 +18,6 @@ open(this_dir + '/data.txt') do |f|
     types = e['types']
     abilities = e['abilities']
     name = e['name']
-    if has_gem?(gems, 'romkan')
-      hira = to_hira name
-      roma = hira.to_roma
-    end
     h = e['H']
     a = e['A']
     b = e['B']
@@ -51,6 +31,6 @@ open(this_dir + '/data.txt') do |f|
     abilities.each.with_index do |ability, j|
       insert_str += ",\\ '特性#{j+1}:\\ #{ability}'"
     end
-    puts "No.#{no}: #{name}(#{roma})\t10new +call\\ append(0,[#{insert_str}])"
+    puts "No.#{no}: #{name}\t10new +call\\ append(0,[#{insert_str}])"
   end
 end
